d1a4076b4ad35b8ade5353592972c36a60681146
Vitor A Oliveira <vitor.a.o.4444@gmail.com>
2024-04-24 23:43:13 -0300

    adicionando arvore de decisao

--- introducao_iris.py
+++ introducao_iris.py
@@ -1,7 +1,10 @@
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import numpy as np
 import scikitplot as skplt
@@ -13,53 +16,93 @@ X = iris.data  # Parâmetros
 y = iris.target  # Rótulos
 
 #Aplicando a validação cruzada
-num_folds = 5  # Número de folds
+num_folds = 4  # Número de folds
 kf = KFold(n_splits=num_folds, shuffle=True)
 
-#Instanciando o classificador
+#Instanciando o classificadores
 k = 5  # Número de vizinhos
 knn = KNeighborsClassifier(n_neighbors=k)
+dtc = DecisionTreeClassifier(random_state=42)
 
 #Vetores para guardar rótulos verdadeiros e probabilidades previstas
 true_labels = []
-predicted_probs = []
+predicted_probs_knn = []
+predicted_probs_dtc = []
 
 for i, (train_index, test_index) in enumerate(kf.split(X)):
+
+    # Separando dados em treino e teste
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
     
+    #KNN treino e previsão
     knn.fit(X_train, y_train)  # Treinando o modelo
-    y_pred = knn.predict(X_test)  # Prevendo rotulos de dados de teste
-    y_prob = knn.predict_proba(X_test)  # Prevendo probabilidade de rotulos de dados de teste
+    y_pred_knn = knn.predict(X_test)  # Prevendo rotulos de dados de teste
+    y_prob_knn = knn.predict_proba(X_test)  # Prevendo probabilidade de rotulos de dados de teste
 
-    #Calculando métricas de avaliação
-    accuracy = accuracy_score(y_test, y_pred)
-    precision_versicolor = precision_score(y_test, y_pred, labels=[1], average=None)[0]
-    recall_versicolor = recall_score(y_test, y_pred, labels=[1], average=None)[0]
-    f1_versicolor = f1_score(y_test, y_pred, labels=[1], average=None)[0]
+    #DTC treino e previsão
+    dtc.fit(X_train, y_train)  # Treinando o modelo
+    y_pred_dtc = dtc.predict(X_test)  # Prevendo rotulos de dados de teste
+    y_prob_dtc = dtc.predict_proba(X_test)  # Prevendo probabilidade de rotulos de dados de teste
+
+    #Calculando métricas de avaliação KNN
+    knn_accuracy = accuracy_score(y_test, y_pred_knn)
+    knn_precision_versicolor = precision_score(y_test, y_pred_knn, labels=[1], average=None)[0]
+    knn_recall_versicolor = recall_score(y_test, y_pred_knn, labels=[1], average=None)[0]
+    knn_f1_versicolor = f1_score(y_test, y_pred_knn, labels=[1], average=None)[0]
+
+    #Calculando métricas de avaliação DTC
+    dtc_accuracy = accuracy_score(y_test, y_pred_dtc)
+    dtc_precision_versicolor = precision_score(y_test, y_pred_dtc, labels=[1], average=None)[0]
+    dtc_recall_versicolor = recall_score(y_test, y_pred_dtc, labels=[1], average=None)[0]
+    dtc_f1_versicolor = f1_score(y_test, y_pred_dtc, labels=[1], average=None)[0]
 
     #Adicionando rótulos verdadeiros e probabilidades previstas
     true_labels.extend(y_test)
-    predicted_probs.extend(y_prob)
+    predicted_probs_knn.extend(y_prob_knn)
+    predicted_probs_dtc.extend(y_prob_dtc)
 
-    #plot das métricas
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    #Plot
+    fig, axes = plt.subplots(2, 3)
 
-    left, width = .25, .5
-    bottom, height = .25, .5
-    right = left + width
-    top = bottom + height
+    #Matriz de confusão KNN
+    skplt.metrics.plot_confusion_matrix(y_test, y_pred_knn, normalize=True, title= f"Matriz de confusão KNN {i}", ax=axes[0, 0])
 
-    skplt.metrics.plot_confusion_matrix(y_test, y_pred, normalize=True, title= f"Matriz de confusão KNN {i}", ax=ax1)
+    #Matriz de confusão DTC
+    skplt.metrics.plot_confusion_matrix(y_test, y_pred_dtc, normalize=True, title= f"Matriz de confusão Arvore de decisão {i}", ax=axes[1, 0])
 
-    ax2.text(0.1, 0.9, f"Acurácia: {accuracy:.2f}", fontsize=12, ha='left', transform=ax2.transAxes)
-    ax2.text(0.1, 0.8, f"Precisão Versicolor: {precision_versicolor:.2f}", fontsize=12, ha='left', transform=ax2.transAxes)
-    ax2.text(0.1, 0.7, f"Recall Versicolor: {recall_versicolor:.2f}", fontsize=12, ha='left', transform=ax2.transAxes)
-    ax2.text(0.1, 0.6, f"F1-score Versicolor: {f1_versicolor:.2f}", fontsize=12, ha='left', transform=ax2.transAxes)
+    #Métricas de avaliação KNN
+    axes[0, 1].text(0.1, 0.9, f"Acurácia: {knn_accuracy:.2f}", fontsize=12, ha='left', transform=axes[0, 1].transAxes)
+    axes[0, 1].text(0.1, 0.8, f"Precisão Versicolor: {knn_precision_versicolor:.2f}", fontsize=12, ha='left', transform=axes[0, 1].transAxes)
+    axes[0, 1].text(0.1, 0.7, f"Recall Versicolor: {knn_recall_versicolor:.2f}", fontsize=12, ha='left', transform=axes[0, 1].transAxes)
+    axes[0, 1].text(0.1, 0.6, f"F1-score Versicolor: {knn_f1_versicolor:.2f}", fontsize=12, ha='left', transform=axes[0, 1].transAxes)
 
-    ax2.axis('off')
+    axes[0, 1].axis('off')
+    axes[0, 1].set_title('Métricas de avaliação KNN')
 
-skplt.metrics.plot_roc(true_labels, predicted_probs)
+    #Métricas de avaliação DTC
+    axes[1, 1].text(0.1, 0.9, f"Acurácia: {dtc_accuracy:.2f}", fontsize=12, ha='left', transform=axes[1, 1].transAxes)
+    axes[1, 1].text(0.1, 0.8, f"Precisão Versicolor: {dtc_precision_versicolor:.2f}", fontsize=12, ha='left', transform=axes[1, 1].transAxes)
+    axes[1, 1].text(0.1, 0.7, f"Recall Versicolor: {dtc_recall_versicolor:.2f}", fontsize=12, ha='left', transform=axes[1, 1].transAxes)
+    axes[1, 1].text(0.1, 0.6, f"F1-score Versicolor: {dtc_f1_versicolor:.2f}", fontsize=12, ha='left', transform=axes[1, 1].transAxes)
+
+    axes[1, 1].axis('off')
+    axes[1, 1].set_title('Métricas de avaliação Arvore de decisão')
+
+    #Visualização
+
+    #KNN
+    clf = Pipeline(
+    steps=[("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=11))]
+    )
+    
+    
+
+    #DTC
+    plot_tree(dtc, ax=[1, 2])
+
+skplt.metrics.plot_roc(true_labels, predicted_probs_knn, title="Curva ROC KNN")
+skplt.metrics.plot_roc(true_labels, predicted_probs_dtc, title="Curva ROC Arvore de decisão")
 
 plt.show()
 
